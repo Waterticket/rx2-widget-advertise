76613968c4f6919d8395591aecef726f6e5f0057
Waterticket <matthew218@naver.com>
2022-11-17 14:34:20 +0000

    first commit

---
+++ advertise.class.php
@@ -0,0 +1,38 @@
+<?php
+
+class advertise extends WidgetHandler
+{
+	function proc($args)
+	{
+		$tpl_path = sprintf('%sskins/%s', $this->widget_path, $args->skin);
+		$tpl_file = 'ads';
+
+		$advertise = \Rhymix\Modules\Advertise\Models\Advertise::getOneActiveAdvertise();
+		if ($args->size == 'big')
+		{
+			$width = 728;
+			$height = 90;
+			$img_url = $advertise->big_img_url;
+		}
+		else
+		{
+			$width = 320;
+			$height = 100;
+			$img_url = $advertise->small_img_url;
+		}
+
+		$click_url = getUrl('','module','advertise','act','dispAdvertiseMoveUrl','advertise_srl',$advertise->advertise_srl);
+		$title = $advertise->title;
+
+		Context::set('title', $title);
+		Context::set('size', $args->size);
+		Context::set('width', $width);
+		Context::set('height', $height);
+		Context::set('image_url', $img_url);
+		Context::set('click_url', $click_url);
+
+		// Compile a template
+		$oTemplate = &TemplateHandler::getInstance();
+		return $oTemplate->compile($tpl_path, $tpl_file);
+	}
+}
