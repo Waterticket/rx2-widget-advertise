5253bdef1bbe26631808ab1fd7e7742852a9a04c
Waterticket <matthew218@naver.com>
2022-11-18 19:16:10 +0000

    텍스트 광고 추가

--- advertise.class.php
+++ advertise.class.php
@@ -8,28 +8,50 @@ class advertise extends WidgetHandler
 		$tpl_file = 'ads';
 
 		$advertise = \Rhymix\Modules\Advertise\Models\Advertise::getOneActiveAdvertise();
+		$advertise_type = $advertise->advertise_type;
 		if ($args->size == 'big')
 		{
 			$width = 728;
 			$height = 90;
-			$img_url = $advertise->big_img_url;
 		}
 		else
 		{
 			$width = 320;
 			$height = 100;
-			$img_url = $advertise->small_img_url;
+		}
+		
+		if ($advertise_type == 'image')
+		{
+			if ($args->size == 'big')
+			{
+				$img_url = $advertise->big_img_url;
+			}
+			else
+			{
+				$img_url = $advertise->small_img_url;
+			}
+		}
+		else if ($advertise_type == 'text')
+		{
+			$img_url = $advertise->thumb_img_url;
+		}
+		else
+		{
+			return;
 		}
 
 		$click_url = getUrl('','mid','ads','act','dispAdvertiseMoveUrl','advertise_srl',$advertise->advertise_srl);
 		$title = $advertise->title;
+		$content = $advertise->content;
 
 		Context::set('title', $title);
+		Context::set('content', $content);
 		Context::set('size', $args->size);
 		Context::set('width', $width);
 		Context::set('height', $height);
 		Context::set('image_url', $img_url);
 		Context::set('click_url', $click_url);
+		Context::set('advertise_type', $advertise_type);
 		Context::set('assign_banner_visible', $args->assign_banner_visible == 'Y');
 
 		// Compile a template
